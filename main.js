04fc36e0a547ffad681b8d6bed1958946917c15f
Dustin Michels <dustin7538@gmail.com>
2022-02-15 00:45:14 -0700

    add share and clear buttons

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-// var app = new Vue({
 Vue.createApp({
-  // el: "#app",
+  created() {
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("poem")) {
+      this.load(urlParams.get("poem"));
+    }
+  },
+
   data() {
     return {
       mode: "compose",
-      modalActive: false,
+      notify: false,
       l1: "",
       l3: "",
       l5: "",
@@ -25,8 +30,30 @@ Vue.createApp({
       l24: "",
     };
   },
-  computed: {
-    wholePoem() {
+
+  methods: {
+    clear() {
+      this.l1 = "";
+      this.l3 = "";
+      this.l5 = "";
+      this.l6 = "";
+      this.l7 = "";
+      this.l9 = "";
+      this.l11 = "";
+      this.l12 = "";
+      this.l13 = "";
+      this.l15 = "";
+      this.l17 = "";
+      this.l18 = "";
+      this.l19 = "";
+      this.l20 = "";
+      this.l21 = "";
+      this.l22 = "";
+      this.l23 = "";
+      this.l24 = "";
+    },
+
+    poemDataToString() {
       return [
         this.l1,
         this.l1,
@@ -56,6 +83,54 @@ Vue.createApp({
         this.l23,
         this.l24,
       ].join("\n");
+    },
+
+    poemStringToData(str) {
+      let lines = str.split("\n");
+      // stanza 1
+      this.l1 = lines?.[0];
+      this.l3 = lines?.[2];
+      this.l5 = lines?.[4];
+      this.l6 = lines?.[5];
+      // stanza 2
+      this.l7 = lines?.[7];
+      this.l9 = lines?.[9];
+      this.l11 = lines?.[11];
+      this.l12 = lines?.[12];
+      // stanza 3
+      this.l13 = lines?.[14];
+      this.l15 = lines?.[16];
+      this.l17 = lines?.[18];
+      this.l18 = lines?.[19];
+      // stanza 4
+      this.l19 = lines?.[21];
+      this.l20 = lines?.[22];
+      this.l21 = lines?.[23];
+      this.l22 = lines?.[24];
+      this.l23 = lines?.[25];
+      this.l24 = lines?.[26];
+    },
+
+    share() {
+      // form url
+      let baseUrl = window.location.href.split("?")[0];
+      let url = baseUrl + "?poem=" + encode(this.poemDataToString());
+      console.log(url);
+      navigator.clipboard.writeText(url);
+      // change current URL
+      window.history.pushState({}, document.title, url);
+      // launch notification
+      this.notify = true;
+    },
+
+    load(poemString) {
+      this.poemStringToData(decode(poemString));
+    },
+  },
+
+  computed: {
+    wholePoem() {
+      return this.poemDataToString();
     },
 
     // --- stanza 1 ---
@@ -129,6 +204,14 @@ Vue.createApp({
 
 // --- Helper functions ---
 
+function encode(str) {
+  return btoa(str);
+}
+
+function decode(encodedString) {
+  return atob(encodedString);
+}
+
 function tokenize(words) {
   words = words.replace(/[^A-Za-z0-9\s]/g, " ");
   let tokens = words.toLowerCase().split(" ");
@@ -140,7 +223,6 @@ function tokenizeStanzas(stanzas) {
   stanzas.forEach((s) => {
     tokens.push(...tokenize(s));
   });
-  // tokens.sort();
   return tokens;
 }
 
