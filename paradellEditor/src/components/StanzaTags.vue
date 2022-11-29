a0c99b54018d1b959f2a67298b1d0c3b4066cb4f
Dustin Michels <dustin7538@gmail.com>
2022-11-29 14:35:25 -0700

    add new style

---
+++ paradellEditor/src/components/StanzaTags.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { findExtra, findUsed, tokenizeLines } from "../helper";
+import { PropType } from "vue";
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+    upperLines: Array as PropType<string[]>,
+    lowerLines: Array as PropType<string[]>,
+  },
+  computed: {
+    upperTokens() {
+      return tokenizeLines(this.upperLines || []);
+    },
+    lowerTokens() {
+      return tokenizeLines(this.lowerLines || []);
+    },
+    usedTokens() {
+      return findUsed(this.upperTokens, this.lowerTokens);
+    },
+    extraTokens() {
+      return findExtra(this.upperTokens, this.lowerTokens);
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="tags">
+    <span
+      v-for="(word, index) in upperTokens"
+      v-bind:class="{ tag: true, 'is-success': usedTokens[index] }"
+    >
+      {{ word }}
+    </span>
+    <span v-for="word in extraTokens" class="tag is-danger">{{ word }}</span>
+  </div>
+</template>
+
+<style scoped></style>
